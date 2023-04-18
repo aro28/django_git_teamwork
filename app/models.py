f9ae7cd962b69e0b336f44a2a9f7e5859c3565a4
aro28 <prostotak28@outlook.com>
2023-04-18 19:56:33 +0600

    added models Position and Employe

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=20)
+    dept = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+class Employee(models.Model):
+    f_name = models.CharField(max_length=20)
+    b_date = models.DateField()
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.f_name
